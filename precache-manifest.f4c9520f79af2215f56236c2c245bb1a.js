be08318ed237e8b103ae293bff82f8f86ad8c385
ruiligao <ruiligao@gmail.com>
2019-04-30 13:05:00 -0700

    Updates

---
+++ precache-manifest.f4c9520f79af2215f56236c2c245bb1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f075340b2188c94dcec6a728cdc2f548",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "6b01f1add3f6058e20f4",
+    "url": "/Clicky-Game/static/css/main.bba73e25.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "6b01f1add3f6058e20f4",
+    "url": "/Clicky-Game/static/js/main.477fb704.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
